537ef7f50cc0cb447e305307f0ba220d6b1b74de
Jascha030 <contact@jaschavanaalst.nl>
2023-08-29 20:01:02 +0200

    🔧 Further build configuration.

--- scoper.php
+++ scoper.php
@@ -1,8 +1,27 @@
 <?php
 
+/**
+ * @noinspection PhpUndefinedNamespaceInspection
+ * @noinspection PhpUndefinedClassInspection
+ */
+
 declare(strict_types=1);
 
+use Isolated\Symfony\Component\Finder\Finder;
+
 return [
-    'prefix'     => 'Bitbucket2Github',
-    'output-dir' => null,
+    'finders' => [
+        Finder::create()->files()->in('src'),
+        Finder::create()
+            ->files()
+            ->ignoreVCS(true)
+            ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/')
+            ->exclude(['doc', 'test', 'test_old', 'tests', 'Tests'])
+            ->in('vendor'),
+        Finder::create()->append(['composer.json']),
+    ],
+    'prefix' => 'Bitbucket2Github',
+    'expose-global-constants' => true,
+    'expose-global-classes'   => true,
+    'expose-global-functions' => true,
 ];
