572685248ab7d3964692cb003d036c2cb309a659
Jascha030 <contact@jaschavanaalst.nl>
2023-09-01 01:11:28 +0200

    🎨 Dunno why I do this, I just like it when it looks clean, and I'm just happy about 1st class callable syntax 🎉

--- scoper.php
+++ scoper.php
@@ -9,19 +9,21 @@ declare(strict_types=1);
 
 use Isolated\Symfony\Component\Finder\Finder;
 
-return [
-    'finders' => [
-        Finder::create()->files()->in('src'),
-        Finder::create()
-            ->files()
-            ->ignoreVCS(true)
-            ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/')
-            ->exclude(['doc', 'test', 'test_old', 'tests', 'Tests'])
-            ->in('vendor'),
-        Finder::create()->append(['composer.json']),
-    ],
-    'prefix' => 'Bitbucket2Github',
-    'expose-global-constants' => true,
-    'expose-global-classes'   => true,
-    'expose-global-functions' => true,
-];
+return (static function (): array {
+    $create = Finder::create(...);
+
+    return [
+        'prefix'  => 'Bitbucket2Github',
+        'finders' => [
+            $create()->files()->in('src'),
+            $create()->files()->in('vendor')
+                ->ignoreVCS(true)
+                ->notName('/LICENSE|.*\\.md|.*\\.dist|Makefile|composer\\.json|composer\\.lock/')
+                ->exclude(['doc', 'test', 'test_old', 'tests', 'Tests']),
+            $create()->append(['composer.json']),
+        ],
+        'expose-global-constants' => true,
+        'expose-global-classes'   => true,
+        'expose-global-functions' => true,
+    ];
+})();
