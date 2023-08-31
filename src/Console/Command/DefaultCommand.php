1993a540c8b389812fe29092a3ed995f79210f25
Jascha030 <contact@jaschavanaalst.nl>
2023-09-01 01:10:34 +0200

    ♻️ Refactor DefaultCommand, split logic blocks into accessors and other methods to keep execute as readable as possible within reason.

--- src/Console/Command/DefaultCommand.php
+++ src/Console/Command/DefaultCommand.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace Jascha030\Bitbucket2Github\Console\Command;
 
+use Generator;
 use InvalidArgumentException;
 use Jascha030\Bitbucket2Github\Executable\Executable;
 use Jascha030\Bitbucket2Github\Executable\Git;
 use Jascha030\Bitbucket2Github\Repository\ComposerRepository;
+use Jascha030\Bitbucket2Github\Repository\GithubRepository;
 use Jascha030\Bitbucket2Github\Repository\RepositoryInterface;
 use RuntimeException;
 use Symfony\Component\Console\Command\Command;
@@ -37,26 +39,30 @@ EOF;
 
     private string $targetDir;
 
-    private RepositoryInterface $repo;
+    private RepositoryInterface $localRepository;
+
+    private RepositoryInterface $targetRepository;
 
     private Git $git;
 
     private Executable $gh;
+
+    private InputInterface $input;
 
     /**
      * @throws \Seld\JsonLint\ParsingException
      */
     public function execute(InputInterface $input, OutputInterface $output): int
     {
-        $directory = $input->getArgument('directory');
-
-        $this->targetDir = match (true) {
-            ! is_string($directory) => throw new InvalidArgumentException('Directory argument must be a string.'),
-            ! is_dir($directory)    => throw new InvalidArgumentException('Directory does not exist.'),
-            default                 => $directory
-        };
-
         $this->validateOrigin();
+
+        $this->input = $input;
+
+        $this->targetRepository = new GithubRepository(
+            $this->getLocalRepository()->getName(),
+            $this->getLocalRepository()->getDescription(),
+            $this->input->hasOption('organization') ? $this->input->getOption('organization') : null
+        );
 
         if (Command::FAILURE === $this->removeOrigin()->mustRun()->getExitCode()) {
             throw new RuntimeException('Could not remove origin remote');
@@ -66,10 +72,8 @@ EOF;
             throw new RuntimeException('Could not create repository');
         }
 
-        $owner = $input->getOption('owner');
-
-        if (is_string($owner)) {
-            $this->createGithubFiles($owner);
+        if (! empty($this->input->getOption('owner'))) {
+            $this->createGithubFiles();
         }
 
         return Command::SUCCESS;
@@ -86,60 +90,18 @@ EOF;
                 'The directory of the repository, defaults to cwd.',
                 getcwd()
             )
-            ->addArgument(
-                'organization',
+            ->addOption(
+                '--organization',
+                '-o',
                 InputArgument::OPTIONAL,
                 'The organization name',
-                'WP-Brothers'
             )
             ->addOption(
-                '--owner',
-                '-o',
+                '--code-owner',
+                '-c',
                 InputArgument::OPTIONAL,
                 'The owner of the repository.'
             );
-    }
-
-    private function createGithubFiles(string $owner): void
-    {
-        if (! str_starts_with($owner, '@')) {
-            $owner = '@' . $owner;
-        }
-
-        $fs = new Filesystem();
-
-        if ($fs->exists($this->targetDir . '/.github/CODEOWNERS')) {
-            return;
-        }
-
-        $fs->mkdir($this->targetDir . '/.github');
-        $fs->touch($this->targetDir . '/.github/CODEOWNERS');
-
-        $fs->dumpFile(
-            $this->targetDir . '/.github/CODEOWNERS',
-            sprintf(self::CONTENT_CODEOWNERS, $owner)
-        );
-    }
-
-    /**
-     * @throws \Seld\JsonLint\ParsingException
-     */
-    private function getRepo(): RepositoryInterface
-    {
-        if (! isset($this->repo)) {
-            $this->repo = new ComposerRepository($this->targetDir);
-        }
-
-        return $this->repo;
-    }
-
-    private function getGit(): Git
-    {
-        if (! isset($this->git)) {
-            $this->git = new Git();
-        }
-
-        return $this->git;
     }
 
     private function getGithubCLI(): Executable
@@ -151,20 +113,45 @@ EOF;
         return $this->gh;
     }
 
-    private function validateOrigin(): void
+    private function getGit(): Git
     {
-        $process = Process::fromShellCommandline($this->getGit() . ' remote -v', $this->targetDir)->mustRun();
-        $lines   = array_filter(explode(PHP_EOL, $process->getOutput()), 'strlen');
-        $lines   = array_filter($lines, static fn (string $line): bool => str_contains($line, 'git@github.com'));
-
-        if (0 !== count($lines)) {
-            throw new RuntimeException('Origin already contains a github remote.');
+        if (! isset($this->git)) {
+            $this->git = new Git();
         }
+
+        return $this->git;
     }
 
-    private function removeOrigin(): Process
+    private function getTargetDir(): string
     {
-        return Process::fromShellCommandline($this->getGit() . ' remote rm origin', $this->targetDir);
+        if (! isset($this->targetDir)) {
+            $directory = $this->input->getArgument('directory');
+
+            $this->targetDir = match (true) {
+                ! is_string($directory) => throw new InvalidArgumentException('Directory argument must be a string.'),
+                ! is_dir($directory)    => throw new InvalidArgumentException('Directory does not exist.'),
+                default                 => $directory
+            };
+        }
+
+        return $this->targetDir;
+    }
+
+    public function getTargetRepository(): RepositoryInterface
+    {
+        return $this->targetRepository;
+    }
+
+    /**
+     * @throws \Seld\JsonLint\ParsingException
+     */
+    private function getLocalRepository(): RepositoryInterface
+    {
+        if (! isset($this->localRepository)) {
+            $this->localRepository = new ComposerRepository($this->getTargetDir());
+        }
+
+        return $this->localRepository;
     }
 
     /**
@@ -172,28 +159,82 @@ EOF;
      */
     private function createRepo(): Process
     {
-        $argString = '';
-        $args      = [
-            '--source'  => $this->targetDir,
-            '--remote'  => 'origin',
-            '--private' => null,
-            '--push'    => null,
-        ];
+        return Process::fromShellCommandline(
+            sprintf(
+                '%s repo create %s %s',
+                $this->getGithubCLI(),
+                $this->getTargetName(),
+                implode(' ', [...($this->getCreateFlags())()])
+            )
+        );
+    }
 
-        if (! empty($this->getRepo()->getDescription())) {
-            $args['--description'] = $this->getRepo()->getDescription();
-        }
+    /**
+     * @throws \Seld\JsonLint\ParsingException
+     */
+    private function getCreateFlags(): Generator
+    {
+        $args = array_filter([
+            '--private'     => true,
+            '--push'        => true,
+            '--remote'      => 'origin',
+            '--source'      => $this->getTargetDir(),
+            '--description' => $this->getLocalRepository()->getDescription(),
+        ]);
 
         foreach ($args as $option => $value) {
-            if (null === $value) {
-                $argString .= $option . ' ';
-            }
+            yield true === $value ? $option : "{$option}=\"{$value}\"";
+        }
+    }
 
-            if (null !== $value) {
-                $argString .= $option . '="' . $value . '" ';
-            }
+    private function validateOrigin(): void
+    {
+        $process = Process::fromShellCommandline($this->getGit() . ' remote -v', $this->getTargetDir())->mustRun();
+
+        $lines = array_filter(
+            array_filter(explode(PHP_EOL, $process->getOutput()), 'strlen'),
+            static fn (string $line): bool => str_contains($line, 'git@github.com')
+        );
+
+        if (0 < count($lines)) {
+            throw new RuntimeException('Origin already contains a github remote.');
+        }
+    }
+
+    private function removeOrigin(): Process
+    {
+        return Process::fromShellCommandline($this->getGit() . ' remote rm origin', $this->getTargetDir());
+    }
+
+    private function createGithubFiles(): void
+    {
+        $owner = $this->input->getOption('owner');
+
+        if (! str_starts_with($owner, '@')) {
+            $owner = '@' . $owner;
         }
 
-        return Process::fromShellCommandline("{$this->getGithubCLI()} repo create WP-Brothers/{$this->getRepo()->getName()} {$argString}");
+        $fs = new Filesystem();
+
+        if ($fs->exists($this->getTargetDir() . '/.github/CODEOWNERS')) {
+            return;
+        }
+
+        $fs->mkdir($this->getTargetDir() . '/.github');
+        $fs->touch($this->getTargetDir() . '/.github/CODEOWNERS');
+
+        $fs->dumpFile(
+            $this->getTargetDir() . '/.github/CODEOWNERS',
+            sprintf(self::CONTENT_CODEOWNERS, $owner)
+        );
+    }
+
+    private function getTargetName(): string
+    {
+        $name = $this->getTargetRepository()->getName();
+
+        return null !== $this->getTargetRepository()->getVendor()
+            ? "{$this->getTargetRepository()->getVendor()}/{$name}"
+            : $name;
     }
 }
