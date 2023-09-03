be3f31a3f6600a6d0e1ebeaded20d03454c9ef4b
Jascha030 <contact@jaschavanaalst.nl>
2023-09-03 02:09:08 +0200

    🐛 Fix InputOption modes.

--- src/Console/Command/DefaultCommand.php
+++ src/Console/Command/DefaultCommand.php
@@ -15,6 +15,7 @@ use RuntimeException;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Process\Process;
@@ -93,13 +94,13 @@ EOF;
             ->addOption(
                 '--organization',
                 '-o',
-                InputArgument::OPTIONAL,
+                InputOption::VALUE_REQUIRED,
                 'The organization name',
             )
             ->addOption(
                 '--code-owner',
                 '-c',
-                InputArgument::OPTIONAL,
+                InputOption::VALUE_REQUIRED,
                 'The owner of the repository.'
             );
     }
